54aeca01465a5e1893df2897f683b079d134ca46
razzkumar <razzkumar.dev@gmail.com>
2020-03-29 08:37:33 +0545

    Initial Workflow

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+# -e will not execute next command if first command is false/error
+# -u will give us a warnig every time we use undeclare variable
+
+ls
+
+yarn
+
+echo "::set-output name=url::http://github.com/razzkumar"
+
+
+exit 0

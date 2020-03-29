fa2fb12c341ef1fd00aaef8c2f23ee80543abf24
razzkumar <razzkumar.dev@gmail.com>
2020-03-29 14:24:29 +0545

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,18 @@ ls
 
 yarn
 
+
+#// file can find on /github/event/event.json
+
+IFS="/" read owner repository <<< "$GIHUB_REPOSITORY"
+
+echo $owner $repository
+
 echo "::set-output name=url::http://github.com/razzkumar"
 
+if [ $? -ne 0 ];then
+  echo "::error::Failed to deployo"
+  exit 1
+fi
 
 exit 0
